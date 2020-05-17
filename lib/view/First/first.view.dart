7900ca0f4ff90809a6b82789352c1b6a2f5bc45b
Ester Mabel <ester.santos@ucsal.edu.br>
2020-05-16 21:31:30 -0300

    botao de Peso e Altura prontos

--- lib/view/First/first.view.dart
+++ lib/view/First/first.view.dart
@@ -13,6 +13,41 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: fvs.appBar(),
+      backgroundColor: fvs.backGroundColor(),
+      body: Stack(
+        children: [
+          ///Container Verde
+          fvs.container1Stack(),
+
+          ///Container Branco
+          fvs.container2Stack(),
+
+          // ///Icon Avatar
+          // fvs.iconAvatarStack(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(padding: EdgeInsets.only(bottom: 40)),
+
+              ///Icon Avatar
+              fvs.iconAvatarStack(),
+              Padding(padding: EdgeInsets.only(bottom: 40)),
+
+              ///Entrada de texto para peso
+              fvs.textField("Weight (Kg)"),
+
+              SizedBox(height: 40,),
+              ///Entrada de texto para altura
+              fvs.textField("Height (Cm)"),
+
+              ///Botão de Calcular
+              ///Texto "Results"
+              ///Saída de texto da classificação do IMC
+              ///Saída do resultado do cálculo
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/view/First/first.view.styles.dart
+++ lib/view/First/first.view.styles.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class FirstViewStyles {
+  ///BackGround Color
+  Color backGroundColor() => Colors.white;
+
+  ///AppBar
   AppBar appBar() {
     String textAppBar = "IMC Calculator";
     Color backgroundColor = Color.fromRGBO(59, 128, 99, 1);
     return AppBar(
-      
       title: Text(
         textAppBar,
       ),
@@ -14,6 +17,70 @@ class FirstViewStyles {
       actions: [
         IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
       ],
+    );
+  }
+
+  ///Container1 Stack
+  Container container1Stack() {
+    Color color = Color.fromRGBO(132, 176, 157, 1);
+    return Container(
+      color: color,
+    );
+  }
+
+  ///Container2 Stack
+  Container container2Stack() {
+    Color color = Colors.white;
+    double height = 138;
+    return Container(
+      height: height,
+      color: color,
+    );
+  }
+
+  ///Icon Avatar Stack
+  Align iconAvatarStack() {
+    Color backgroundColor = Color.fromRGBO(44, 93, 72, 1);
+    Color colorIcon = Colors.white;
+    double iconSize = 50.0;
+    return Align(
+      alignment: Alignment(0, -0.74),
+      child: CircleAvatar(
+        backgroundColor: backgroundColor,
+        radius: iconSize,
+        child: Icon(
+          Icons.fitness_center,
+          color: colorIcon,
+          size: iconSize,
+        ),
+      ),
+    );
+  }
+
+  ///Entrada de dados para Peso
+  Container textField(String hintText) {
+    double height = 45;
+    double width = 370;
+    Color containerColor = Colors.white;
+    Color hintTextColor = Colors.grey;
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: containerColor,
+        borderRadius: BorderRadius.circular(27),
+      ),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 20),
+          border: InputBorder.none,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: hintTextColor,
+          ),
+        ),
+      ),
     );
   }
 }
